53d1552c8bc6a57b1e0c9eda68a06b2d45560f68
Michael John D. Ramos <mjramos427@gmail.com>
2026-04-01 02:55:11 +0800

    chore: installed packages for backend, added dependencies for frontend.

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,13 @@
-const express = require("express");
+const express = require('express'); // for easier api building
+const cors = require('cors');
+require('dotenv').config();
 
-// test
+// testing code
+
 const app = express();
+app.use(cors()); // allows flutter to call api
+app.use(express.json());
 
-port = 3000;
-
-app.listen(port, () =>{
-    console.log(`Successfully connected to ${port}`)
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
 });
