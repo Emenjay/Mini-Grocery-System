371319c05c0b031aa34f495771f47c1110460348
Michael John D. Ramos <mjramos427@gmail.com>
2026-04-02 20:00:08 +0800

    chore: created shifts table schema

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -12,4 +12,17 @@ CREATE TABLE IF NOT EXISTS users (
     Username VARCHAR(50) NOT NULL UNIQUE,
     PinCodeHash VARCHAR(255) NOT NULL,
     IsActive BOOLEAN DEFAULT FALSE
-)
+);
+
+-- Create attendance_shifts table
+CREATE TABLE IF NOT EXISTS attendance_shifts (
+    ShiftID INT AUTO_INCREMENT PRIMARY KEY,
+    UserID INT NOT NULL,
+    Time_In DATETIME NOT NULL,
+    StartingCash DECIMAL(10,2) NOT NULL,
+    Time_Out DATETIME,
+    EndingCash DECIMAL(10,2),
+    TotalSales DECIMAL(10,2),
+
+    FOREIGN KEY (UserID) REFERENCES users(UserID)
+);
