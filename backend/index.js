cee79bdb8272331d39de38c3119ee93056f41433
Michael John D. Ramos <mjramos427@gmail.com>
2026-04-02 19:21:53 +0800

    feat: built login auth api

--- backend/index.js
+++ backend/index.js
@@ -3,17 +3,24 @@ const cors = require('cors');
 require('dotenv').config();
 const db = require('./config/db'); // import db config
 
+const authRoutes = require('./routes/authRoutes'); // import auth route
+
 // testing code
 
 const app = express();
 app.use(cors()); // allows flutter to call api
 app.use(express.json());
 
+// login auth
+app.use('/api/auth', authRoutes);
+
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
 
+
+
 // Test query
-db.query('SELECT 1')
-  .then(() => console.log('✅ MySQL Connected!'))
-  .catch(err => console.error('❌ DB Error:', err));
+// db.query('SELECT 1')
+//   .then(() => console.log('✅ MySQL Connected!'))
+//   .catch(err => console.error('❌ DB Error:', err));
