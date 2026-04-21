3d40b9d0860a78dfeaa951e549e1b14708f4bfe8
Michael John D. Ramos <mjramos427@gmail.com>
2026-04-22 03:26:36 +0800

    chore: added protect import in authRoute

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
+const protect = require('../middleware/authMiddleware');
 
 // login route
 router.post('/login', authController.login);
